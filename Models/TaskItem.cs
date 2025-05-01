e5dedb28456ca70579ea2c03eb8475ea579ca0dd
Ricarpell <ricardopellegri@gmail.com>
2025-05-01 17:25:39 -0300

    Update TaskItem.cs

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,4 +1,4 @@
-﻿using MongoDB.Bson;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.ComponentModel.DataAnnotations;
 
@@ -12,10 +12,12 @@ namespace TaskManagerApi.Models
 
         [BsonElement("title")]
         [Required(ErrorMessage = "El título es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El título no puede exceder 100 caracteres.")]
         public string Title { get; set; } = string.Empty;
 
         [BsonElement("description")]
         [Required(ErrorMessage = "La descripción es obligatoria.")]
+        [StringLength(500, ErrorMessage = "La descripción no puede exceder 500 caracteres.")]
         public string Description { get; set; } = string.Empty;
 
         [BsonElement("isCompleted")]
